c9e23d9db36fa90cfacebc19ab07f3702e34f744
degD <108623049+degD@users.noreply.github.com>
2022-10-19 21:15:34 +0300

    Trying a new way to optimize the code

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -8,6 +8,44 @@ def SudokuSolver(puzzle):
     
     def square_coords(y, x):
         return (y // 3), (x // 3)
+    
+    def movement(y, x):
+    
+        if ((x+1) % 9 == 0) and ((y+1) % 3 == 0):
+            x = 0
+            y += 1
+        
+        elif ((x+1) % 3 == 0) and ((y+1) % 3 == 0):
+            x += 1
+            y -= 2
+
+        elif ((x+1) % 3 != 0):
+            x += 1
+        
+        elif ((x+1) % 3 == 0):
+            x -= 2
+            y += 1
+            
+        return y, x
+
+    def movementr(y, x):
+        
+        if (x % 9 == 0) and (y % 3 == 0):
+            x = 8
+            y -= 1
+        
+        elif (x % 3 == 0) and (y % 3 == 0):
+            x -= 1
+            y += 2
+
+        elif (x % 3 != 0):
+            x -= 1
+        
+        elif (x % 3 == 0):
+            x += 2
+            y -= 1
+            
+        return y, x
 
     # Defining some values prior to start
     puzzle_3x3_data = [list() for _ in range(9)]
